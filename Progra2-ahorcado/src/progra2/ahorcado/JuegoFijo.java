c7a53d5e0e06ae48cbf9956c1fc8bcb648e894b3
ALISSON RAQUEL MARTINEZ SERRANO <alisson_martinez@unitec.edu>
2026-02-13 10:33:43 -0600

    jeugoazar

--- Progra2-ahorcado/src/progra2/ahorcado/JuegoFijo.java
+++ Progra2-ahorcado/src/progra2/ahorcado/JuegoFijo.java
@@ -8,6 +8,44 @@ package progra2.ahorcado;
  *
  * @author ALISSONRAQUELMARTINE
  */
-public class JuegoFijo {
+public abstract class JuegoFijo extends JuegoBase {
+ 
+    public JuegoFijo(String palabra){
+        this.PalabraSecreta = palabra.toLowerCase();
+        this.PalabraActual = "_".repeat(PalabraSecreta.length());
+    }
+
+    @Override
+    protected boolean Verificar(char letra) {
+     
+        return PalabraSecreta.indexOf(letra)>=0;   
+    }
+
+    @Override
+    protected void actualizarPalabra(char letra) {
+        
+        System.out.println("Palabra secreta estableciada" + "*".repeat(PalabraSecreta.length()));
+    }
+
+    protected boolean Ganador(char letra) {
     
+        return PalabraActual.equals(PalabraSecreta);
+    }
+
+    
+    protected void inicializarPalabraSecreta(char letra) {
+     
+        String palabra ="";
+        
+        for (int i =0 ; i<PalabraSecreta.length(); i++){
+            if (PalabraSecreta.charAt(i)== letra){
+                palabra += letra;
+            } else{
+                palabra += PalabraActual.charAt(i);
+            }
+        }
+        PalabraActual = palabra;
+    }
+
+   
 }
