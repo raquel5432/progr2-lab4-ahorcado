015a2e4f8f620f8549d371568349134ea80fd38e
ALISSON RAQUEL MARTINEZ SERRANO <alisson_martinez@unitec.edu>
2026-02-13 09:50:00 -0600

    juego base

--- Progra2-ahorcado/src/progra2/ahorcado/JuegoBase.java
+++ Progra2-ahorcado/src/progra2/ahorcado/JuegoBase.java
@@ -5,14 +5,17 @@
 package progra2.ahorcado;
 
 import java.util.ArrayList;
+import progra2.ahorcado.Exceptions.LetraInvalidaException;
+import progra2.ahorcado.Exceptions.LetraRepetidaException;
+import progra2.ahorcado.Exceptions.SinIntentosException;
 
 /**
  *
  * @author ALISSONRAQUELMARTINE
  */
 public abstract class JuegoBase implements Ahorcable {
- protected String Secreta;
- protected String Actual;
+ protected String PalabraSecreta;
+ protected String PalabraActual;
  protected int intentos;
  protected final int Limites = 6;
  protected ArrayList<Character> LetrasUsadas;
@@ -25,82 +28,147 @@ public abstract class JuegoBase implements Ahorcable {
  
  public String PrintFigura(int intentos){
      
-     switch(intentos){
-         
-         case 0: return " +---+"
-                    +    "\n |   |"
-                    +    "\n     |"
-                    +    "\n     |"
-                    +    "\n     |"
-                    +    "\n     |"
-                    +    "\n=========";
-         
-         case 1: return " +---+"
-                    +    "\n  |   |"
-                    +    "\n      |"
-                    +    "\n      |"
-                    +    "\n      |"
-                    +    "\n      |"
-                    +    "\n=========";
-             
-         case 2: return " +---+"
-                    +    "\n  |   |"
-                    +    "\n  O   |"
-                    +    "\n      |"
-                    +    "\n      |"
-                    +    "\n      |"
-                    +    "\n=========";
-         
-         case 3: return " +---+"
-                    +    "\n  |   |"
-                    +    "\n  O   |"
-                    +    "\n  |   |"
-                    +    "\n      |"
-                    +    "\n      |"
-                    +    "\n=========";
-         
-         case 4: return " +---+"
-                    +    "\n  |   |"
-                    +    "\n  O   |"
-                    +    "\n /|   |"
-                    +    "\n      |"
-                    +    "\n      |"
-                    +    "\n=========";
-         
-         case 5: return " +---+"
-                    +    "\n  |   |"
-                    +    "\n  O   |"
-                    +    "\n /|\\ |"
-                    +    "\n      |"
-                    +    "\n      |"
-                    +    "\n=========";
-         
-         case 6: return " +---+"
-                    +    "\n  |   |"
-                    +    "\n  O   |"
-                    +    "\n /|\\ |"
-                    +    "\n      |"
-                    +    "\n      |"
-                    +    "\n=========";
-         
-         case 7: return " +---+"
-                    +    "\n  |   |"
-                    +    "\n  O   |"
-                    +    "\n /|\\ |"
-                    +    "\n /    |"
-                    +    "\n      |"
-                    +    "\n=========";
-       
-         case 8: return " +---+"
-                    +    "\n  |   |"
-                    +    "\n  O   |"
-                    +    "\n /|\\ |"
-                    +    "\n / \\ |"
-                    +    "\n      |"
-                    +    "\n=========";
-         
-         case 9: return 
-             
-     }
+     return switch (intentos) {
+         case 0 -> """
+                    +---+
+                    |   |
+                        |
+                        |
+                        |
+                        |
+                   =========""";
+         case 1 -> """
+                    +---+
+                     |   |
+                         |
+                         |
+                         |
+                         |
+                   =========""";
+         case 2 -> """
+                    +---+
+                     |   |
+                     O   |
+                         |
+                         |
+                         |
+                   =========""";
+         case 3 -> """
+                    +---+
+                     |   |
+                     O   |
+                     |   |
+                         |
+                         |
+                   =========""";
+         case 4 -> """
+                    +---+
+                     |   |
+                     O   |
+                    /|   |
+                         |
+                         |
+                   =========""";
+         case 5 -> """
+                    +---+
+                     |   |
+                     O   |
+                    /|\\ |
+                         |
+                         |
+                   =========""";
+         case 6 -> """
+                    +---+
+                     |   |
+                     O   |
+                    /|\\ |
+                         |
+                         |
+                   =========""";
+         case 7 -> """
+                    +---+
+                     |   |
+                     O   |
+                    /|\\ |
+                    /    |
+                         |
+                   =========""";
+         case 8 -> """
+                    +---+
+                     |   |
+                     O   |
+                    /|\\ |
+                    / \\ |
+                         |
+                   =========""";
+         case 9 -> """
+                    +---+
+                     |   |
+                     O   |
+                    /|\\ |
+                    / \\  |
+                         |
+                   =========""";
+         default -> """
+                     +---+
+                      |   |
+                      O   |
+                     /|\\ |
+                     / \\ |
+                          |
+                    =========""";
+     };     
+
  }
+ 
+    public String getPalabraActual(){
+    
+        return PalabraActual;
+} 
+    public String getPalabraSecreta(){
+        
+        return PalabraSecreta;
+ }
+    
+   public void Validar(char letra) throws LetraInvalidaException, LetraRepetidaException {
+
+    letra = Character.toLowerCase(letra);
+
+    if (!Character.isLetter(letra)) {
+        throw new LetraInvalidaException("SOLO LETRAS DEL ALFABETO");
+    }
+
+    if (LetrasUsadas.contains(letra)) {
+        throw new LetraRepetidaException("La letra " + letra + " ya fue usada");
+    }
 }
+
+   
+   public boolean play(char letra)throws LetraRepetidaException, LetraInvalidaException, SinIntentosException{
+       
+       Validar(letra);
+       
+       LetrasUsadas.add(letra);
+       
+       if (Verificar(letra)){
+           
+           actualizarPalabra(letra);
+           return true;
+       } else{
+           intentos++;
+           if(intentos >= Limites){
+               throw new SinIntentosException("Has perdido. La palabra era: " + PalabraSecreta);
+           
+           } return false;
+       }
+       
+   }
+    protected abstract boolean Verificar(char letra);
+         
+    protected abstract void actualizarPalabra(char letra);
+    
+    protected abstract boolean Ganador();
+    
+        
+}
+
