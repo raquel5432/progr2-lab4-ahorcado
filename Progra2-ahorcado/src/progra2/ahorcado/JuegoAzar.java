e76058bfdcbfd398376773c281dc22955045b953
ALISSON RAQUEL MARTINEZ SERRANO <alisson_martinez@unitec.edu>
2026-02-13 10:12:30 -0600

    juegoAzar

--- Progra2-ahorcado/src/progra2/ahorcado/JuegoAzar.java
+++ Progra2-ahorcado/src/progra2/ahorcado/JuegoAzar.java
@@ -4,10 +4,66 @@
  */
 package progra2.ahorcado;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import progra2.ahorcado.Exceptions.SinIntentosException;
+
 /**
  *
  * @author ALISSONRAQUELMARTINE
  */
-public class JuegoAzar {
+public abstract class JuegoAzar extends JuegoBase{
     
+    private final AdminPalabrasSecretas admin;
+
+    public JuegoAzar(AdminPalabrasSecretas admin) {
+        this.admin = admin;
+        inicializarPalabraSecreta();
+    }
+
+    @Override
+    protected boolean Verificar(char letra) {
+        
+        return PalabraSecreta.indexOf(letra) >=0;
+        
+        }
+
+    @Override
+    protected void actualizarPalabra(char letra) {
+        
+        String palabra = "";
+        
+        for(int i=0 ; i<PalabraSecreta.length(); i++){
+            if (PalabraSecreta.charAt(i)== letra){
+                palabra += letra;
+            }else {
+                palabra +=PalabraActual.charAt(i);
+            }
+        }
+        PalabraActual = palabra;
+        
+    }
+
+    @Override
+    protected boolean Ganador() {
+        
+        return PalabraActual.equals(PalabraSecreta);
+        
+    }
+
+    @Override
+    public void inicializarPalabraSecreta() {
+        
+        try {
+            this.PalabraSecreta = admin.obtenerPalabraAleatoria().toLowerCase();
+            
+            this.PalabraActual = "_".repeat(PalabraSecreta.length());
+            
+            this.intentos = 0;
+        } catch (SinIntentosException ex) {
+            Logger.getLogger(JuegoAzar.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+  
 }
